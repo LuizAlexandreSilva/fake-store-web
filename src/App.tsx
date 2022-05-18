f0aba02ed0829edc63b5f660648d3e815a50a787
Alexandre Silva <alexandre.silva@meliuz.com.br>
2022-05-18 18:06:48 -0400

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Home } from './components/pages/Home';
+import { NoPageFound } from './components/pages/NoPageFound';
+import { Product } from './components/pages/Product';
 
 function App() {
-  return <h1 className="text-3xl">Hello world</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="*" element={<NoPageFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
