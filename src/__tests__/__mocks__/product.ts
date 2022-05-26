4ab5104cd1e098d58eec1b53025cca845f6053e4
Alexandre Silva <alexandre.silva@meliuz.com.br>
2022-05-25 21:31:11 -0400

    tests: CartItem component

---
+++ src/__tests__/__mocks__/product.ts
@@ -0,0 +1,15 @@
+import { Product } from '../../entities/product';
+
+export const productMock: Product = {
+  id: 1,
+  title: 'T-shirt',
+  description: "Men's white T-shirt",
+  category: "men's clothing",
+  image: 'img-src',
+  price: 29.9,
+  rating: {
+    rate: 4.5,
+    count: 10,
+  },
+  quantity: 2,
+};
